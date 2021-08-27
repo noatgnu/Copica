6d98f5ef7d88d7780f7be7693c6f43255fdde359
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2021-08-27 18:18:46 +0100

    update

--- src/app/service/uniprot.service.ts
+++ src/app/service/uniprot.service.ts
@@ -8,7 +8,7 @@ import {BehaviorSubject, Observable, Subject} from "rxjs";
   providedIn: 'root'
 })
 export class UniprotService {
-  private baseURL = 'http://www.uniprot.org/uploadlists/?';
+  private baseURL = 'https://www.uniprot.org/uploadlists/?';
   public Re = /[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}/;
 
   private base = 'https://www.uniprot.org';
